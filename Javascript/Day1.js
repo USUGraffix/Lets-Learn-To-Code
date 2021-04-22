a49dec5b492b674e393e4cd41a0f18300fa79a65
Stewart Mckenzie <mckenzie.stewart.e@gmail.com>
2021-04-22 16:00:48 -0700

    First javascript lesson

---
+++ Javascript/Day1.js
@@ -0,0 +1,55 @@
+// Programing is just writing instructions for your computer to complete
+
+//Each instruction you write when you program is called a statement
+
+//Statements always end in a semicolon
+let peterGatsby;
+
+//The statement DECLARES a variable
+//Variables are containers that store values/data types
+//peterGatsby is a variable created using the key word let.
+
+//The key word let tells the computer "I want to make a variable who's value can change"
+//Right now the peterGatsby variable has no value
+
+//but we can use operators and data types to give it a value
+
+peterGatsby = 'A punk sucks';
+
+// the line above uses the "=" operator to assign peterGatsby equal to a string containing "A punk b#$^#@"
+
+//"=" is a operator used to assign values or data types
+// when you use a single "=" your are assigning a variable on the left to a value or data type on the right
+
+//Here DandyReyes is assigned to  the string "A birdman"
+let dandyReyes = 'A birdman';
+
+//There are many keywords other than let,many operators other than "=" ,and many data types other than strings for example
+
+const margsHouseCaughtFire = 9 + 10;
+
+//const is a keyword that tells the computer "I want to make a variable who's value cannot change"
+
+//to the right of the "=" we see 9+10
+//here we are adding two number data types by using the "+" operator
+//A single + between two numbers adds the numbers as you would think
+//You can also + two different data types but the result may not always be what you think
+//The other math symbols also function as operators when coding "-","/", and "*"
+
+//you can also use variables in place of "raw" data types
+let alisonsBirdCount = 3;
+let dandysBirdCount = 9001;
+let totalAmountOfBirds = alisonsBirdCount + dandysBirdCount;
+
+//We assigned totalAmountOfBirds to the sum of alisonsBirdCount + dandysBirdCount;
+
+//if we wanted to have this value logged to our terminal we would use something called a function 
+//console.log() in javascript prints out what ever you pass to its parenthesis  your terminal
+
+//pass alisonsBirdCount  to console.log()
+//pass totalAmountOfBirds to console.log()
+//pass a string explaining the amount + the variable holding the amount to console.log()
+
+
+
+
